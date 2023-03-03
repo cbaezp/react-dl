14249ce338888e549eb2fda51256eaad2d1bcdab
Cesar <cbaezp7@gmail.com>
2023-03-03 15:51:05 -0700

    add link to repo

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ function App() {
             </p>
             <div className="mt-8">
               <a
-                href="#"
+                href="https://github.com/cbaezp/react-dl"
                 className="inline-flex rounded-md bg-white/10 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-white/20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white hover:bg-cyan-500"
               >
                 Github
